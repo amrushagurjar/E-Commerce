780c569d1022c2d567f2084f17108418ba7c4bf9
amrushagurjar <amrushagurjar1818@gmail.com>
2024-11-25 00:06:54 +0530

    fixed bug

--- demo/src/main/java/apitestime/example/demo/entity/User.java
+++ demo/src/main/java/apitestime/example/demo/entity/User.java
@@ -1,10 +1,7 @@
 package apitestime.example.demo.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +13,7 @@ public class User {
     @Column(name = "email")
     public String email;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "first_name")
     private String firstName;
